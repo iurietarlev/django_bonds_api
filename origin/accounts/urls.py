604b5ffc60df0340cad4f586bacf00d3ef917005
Iurie Tarlev <iura.tarlev@gmail.com>
2020-09-27 00:27:12 +0100

    🔨 added user/ url

--- origin/accounts/urls.py
+++ origin/accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('auth/', include('knox.urls')),
     path('register/', RegisterAPI.as_view()),
     path('login/', LoginAPI.as_view()),
+    path('user/', UserAPI.as_view()),
     path('logout/', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
