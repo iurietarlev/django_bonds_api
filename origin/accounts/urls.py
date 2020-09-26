657e19d722fc9b3d9f715d52a3e5657db9c2789d
Iurie Tarlev <iura.tarlev@gmail.com>
2020-09-26 23:55:59 +0100

    🔨 fixed urls

--- origin/accounts/urls.py
+++ origin/accounts/urls.py
@@ -3,8 +3,8 @@ from .api import RegisterAPI, LoginAPI, UserAPI
 from knox import views as knox_views
 
 urlpatterns = [
-    path('auth', include('knox.urls')),
-    path('register', RegisterAPI.as_view()),
-    path('login', LoginAPI.as_view()),
-    path('logout', knox_views.LogoutView.as_view(), name='knox_logout')
+    path('auth/', include('knox.urls')),
+    path('register/', RegisterAPI.as_view()),
+    path('login/', LoginAPI.as_view()),
+    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
